51e8ecc292b4e425f7299f33cda19e6fd4f8f86f
Vaibhav Krishna U <vaibhavkrishna0607@gmail.com>
2025-01-14 09:32:31 +0530

    uploading

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      Roboto: ["Roboto","arial"],
+      Poppins: ['Poppins',"arial"],
+    }
+  },
+  extend: {
+    screens: {
+      "1000px": "1050px",
+      "1100px": "1110px",
+      "800px": "13000px",
+      "400px": "400px",
+    }
   },
   plugins: [],
 }
