56268b29f989c8341e08f2b0952c7210e32e78df
VaibhavKrishna0607 <vaibhavkrishna0607@gmail.com>
2025-01-16 17:06:31 +0530

    Delete src directory

--- src/Routes/Route.js
+++
@@ -1,6 +0,0 @@
-//eslint-disable-next-line
-// import React from "react";
-import LoginPage from "../pages/login.jsx"
-import SignupPage from "../pages/signupPage.jsx";
-
-export {LoginPage,SignupPage}
