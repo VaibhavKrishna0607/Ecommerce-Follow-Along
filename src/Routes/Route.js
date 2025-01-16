d038d14647ca76a1bb64b7be4d796c2f02882203
Vaibhav Krishna U <vaibhavkrishna0607@gmail.com>
2025-01-16 13:55:50 +0530

    ml5

--- src/Routes/Route.js
+++ src/Routes/Route.js
@@ -1,5 +1,6 @@
 //eslint-disable-next-line
-import React from "react";
+// import React from "react";
 import LoginPage from "../pages/login.jsx"
+import SignupPage from "../pages/signupPage.jsx";
 
-export {LoginPage,}
+export {LoginPage,SignupPage}
