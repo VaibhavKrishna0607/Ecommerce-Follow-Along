f88ea563e5ea190e2f32f0dcf3e9f0f34994aa4b
Vaibhav Krishna U <vaibhavkrishna0607@gmail.com>
2025-01-16 17:11:37 +0530

    added

---
+++ Frontennd1/src/Routes/Route.js
@@ -0,0 +1,6 @@
+//eslint-disable-next-line
+// import React from "react";
+import LoginPage from "../pages/login.jsx"
+import SignupPage from "../pages/signupPage.jsx";
+
+export {LoginPage,SignupPage}
