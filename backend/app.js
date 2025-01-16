0d7b5162f591458c912dba2c1dec3c1a7a08c733
Vaibhav Krishna U <vaibhavkrishna0607@gmail.com>
2025-01-16 17:05:32 +0530

    changed

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
+const user = require("./controller/userController");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const errorHandler = require("./middleware/error");
 
-if(process.env.NODE_ENV !== "PRODUCTION"){
-    require("dotenv").config({
-        path: "backend/config/.env"
-    });
-};
+app.use(errorHandler);
 
-app.get('/', (req, res) => {
-    return res.send("Welcome to the backend");
-})
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors());
+
+app.use("/api/v2/user", user);
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config.env",
+  });
+}
+
+app.get("/", (_req, res) => {
+  return res.send("Welcome to backend");
+});
 
 module.exports = app;
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,19 @@
-const app = require("./app")
-const connectDB = require("./database/Database");
+const app = require("./app");
+const connectDatabase = require("./db/Database");
 
-process.on("uncaughtException", (err) =>{
+process.on("uncaughtException", (err)=>{
     console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server for handling uncaught exception`)
-})
+    console.log(`Shutting down server`);
+});
 
-if (process.env.NODE_ENV !== "PRODUCTION"){
+if(process.env.NODE_ENV !== "PRODUCTION"){
     require("dotenv").config({
         path: "config/.env",
     });
 };
 
-connectDB();
+connectDatabase();
 
 const server = app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on https://localhost:${process.env.PORT}`)
+    console.log(`Server is running on https://localhost:${process.env.PORT}`);
 })
-
-// xWLcUv2KbLhI62oK
